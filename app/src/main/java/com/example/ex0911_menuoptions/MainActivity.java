887a4e532d800157ee09cb097e1bb6277d74dead
ay4748 <ay4748@bs.amalnet.k12.il>
2024-12-17 20:03:37 +0200

    current

--- app/src/main/java/com/example/ex0911_menuoptions/MainActivity.java
+++ app/src/main/java/com/example/ex0911_menuoptions/MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.ex0911_menuoptions;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et1,et2;
+    TextView tVans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initviews();
+    }
+
+    private void initviews() {
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        tVans = findViewById(R.id.tVans);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu (menu);
+    }
+
+    public boolean canAct(String st) {
+        if (st.isEmpty() || st.equals("-") || st.equals(".") || st.equals("+") || st.equals("-.") || st.equals("+.")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean onOptionsItemSelected(@Nullable MenuItem item)
+    {
+
+
+        return true;
     }
 }
